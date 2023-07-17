3ff4b4548d56a9f3b086d1876b08d7f0586a13ce
ThereAre12Months <rubendriezen@gmail.com>
2023-07-17 12:30:05 +0200

    fix bug

--- docs/editor.py
+++ docs/editor.py
@@ -2,4 +2,4 @@ def buttonClick():
     print("this works")
     print("output", executeCode('out("Hello!")'))
 
-print("v1.3")
+print("v1.4")
--- docs/mpl.py
+++ docs/mpl.py
@@ -2,7 +2,7 @@ def executeCode(code):
     import contextlib, io
 
     setGlobals()
-    txt = compile(code, False, isCode=True)
+    txt = compile(code, False, doWrite=False, isCode=True)
     f = io.StringIO()
     with contextlib.redirect_stdout(f):
         interpret(txt, True)
